938d9e957bd9797cb16fcbc61d5560b0e4e83bc6
RohanRadia <rohan.radia@icloud.com>
2019-12-03 14:34:05 +0000

    [day 1] [test 1 + test 2]

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,13 +1,18 @@
-import os
-import sys
-
 # Procedure is:
 # (Round Down(Initial number / 3))-2
 
-masses = []
+fuel = 0
+extra_req = 0
 
 file = input("File name: ")
 
 with open(file, "r") as f:
     for line in f:
-        masses.append(line.split("\n")[0])
+        extra_req = (int((int(line.split("\n")[0]))/3))-2
+
+        while extra_req > 0:
+            fuel += extra_req
+
+            extra_req = (int(extra_req/3))-2
+
+print(f"Fuel needed: {fuel}")
